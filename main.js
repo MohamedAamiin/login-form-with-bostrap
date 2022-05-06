7403960b4caa697c4cb2ceb8840a2ed7aee0d0bb
Mohamed aamiin <engmohammedaamiin.com>
2022-05-06 13:33:00 +0300

    login form using boostrap

---
+++ main.js
@@ -0,0 +1,12 @@
+function login(){
+const username='sicid'
+const password=333;
+
+if(username==='admin',password===123){
+    console.log('login sucessfully');
+}
+else{
+    console.log('login failed')
+}
+}
+login();
